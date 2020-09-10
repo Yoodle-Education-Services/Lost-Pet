c8f07010b3e8d3bd03b808e916e730c79cd394e8
maraton25 <67726802+maraton25@users.noreply.github.com>
2020-09-10 14:19:21 -0500

    I changed the home page of the Angular
    I uppdated the home for the Angular side. I changed the background

--- app_public/src/app/home/home.component.ts
+++ app_public/src/app/home/home.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validator } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
+import{ FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  userInformationForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    petName: new FormControl(''),
-    breed: new FormControl(''),
-    description: new FormControl(''),
-    location: new FormControl(''),
-
-  });
-
 
   constructor() { }
 
