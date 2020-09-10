df8b8ef043958c32abce2b5d7ecc98fb2323d8a3
maraton25 <67726802+maraton25@users.noreply.github.com>
2020-09-10 10:42:53 -0500

    updated the app.js for allowing access and adding cores to use it

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const host = process.env.DB_HOST || 'mongodb://127.0.0.1/petcustomers';
 const dbURL = `${host}`;
 const redline = require('readline');
 
+const cors = require('cors');
+
+
 require('./models/petcustomers');
 require('dotenv').config();
 
@@ -52,6 +55,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', petcustomersRouter);
 
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -67,5 +80,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
